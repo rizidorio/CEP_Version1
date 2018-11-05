932141ec6822f1491f6b54a6446ddba754eb0ec7
rizidorio <rferreira013@hotmail.com>
2018-11-05 15:21:57 -0200

    Versão 1 App CEP

---
+++ CEP/CEP/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using CEP.Servico.Modelo;
+using CEP.Servico;
+
+
+namespace CEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBuscarCep.Clicked += BuscarEndereco;
+        }
+
+        private void BuscarEndereco(object sender, EventArgs e)
+        {
+            string cep = txtCep.Text.Trim();
+
+            if (IsValid(cep))
+            {
+                try
+                {
+                    Endereco endereco = ViaCEPServico.BuscarEndereco(cep);
+
+                    if(endereco.cep != null)
+                    {
+                        lblCep.Text = "CEP: " + endereco.cep;
+                        lblLogradouro.Text = "Logradouro: " + endereco.logradouro;
+                        lblBairro.Text = "Bairro: " + endereco.bairro;
+                        lblCidade.Text = "Cidade: " + endereco.localidade;
+                        lblEstado.Text = "Estado: " + endereco.uf;
+
+                        txtCep.Text = "";
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O não foi encontrado endereço para o CEP: " + cep, "OK");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("ERRO CRÍTICO", ex.Message, "OK");
+                }
+            }
+        }
+
+        private bool IsValid(string cep)
+        {
+            bool valido = true;
+
+            if(cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "O CEP deve conter 8 caracteres", "OK");
+
+                valido = false;
+            }
+
+            if(!int.TryParse(cep, out int NovoCep))
+            {
+                DisplayAlert("ERRO", "O CEP deve conter somente números", "OK");
+
+                valido = false;
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ CEP/CEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using CEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+
+namespace CEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string URL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEndereco(string cep)
+        {
+            string NovaURL = string.Format(URL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovaURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+        }
+    }
+}
